deaa5cb17f2fef4741b25b7a68d5860accd87db2
ouyong <1711216459@qq.com>
2020-08-09 15:29:34 +0800

    ouyong添加了一行代码

--- atcrowdfunding-common/src/main/java/com/atguigu/atcrowdfunding/GitTest.java
+++ atcrowdfunding-common/src/main/java/com/atguigu/atcrowdfunding/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好GitHub！！！master");
         System.out.println("你好GitHub！！！dev...");
+        System.out.println("你好ouyong!!!");
     }
 }
