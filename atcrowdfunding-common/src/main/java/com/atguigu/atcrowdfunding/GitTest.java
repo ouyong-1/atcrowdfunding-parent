9cb737bfea33eed6b82284a7e0b5a9756a967b52
ouyong <1711216459@qq.com>
2020-08-09 12:20:35 +0800

    新增GitHello类

---
+++ atcrowdfunding-common/src/main/java/com/atguigu/atcrowdfunding/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.atcrowdfunding;
+
+/**
+ * @Author: ouyong
+ * @Date: 2020/08/09 12:18
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("你好GitHub！！！");
+    }
+}
